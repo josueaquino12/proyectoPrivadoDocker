af6ebe394d58a5248bf57f040d38546e801864ba
josueaquino12 <49490829+josueaquino12@users.noreply.github.com>
2022-08-17 22:22:08 -0300

    1er commit proyecto docker
    1er commit proyecto docker

---
+++ api/registro-accesos/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/hola', (req, res) => {
+    console.log('GET /hola...')
+    res.send('Hola mundo desde ExpressJS!')
+})
+
+app.listen(PORT, () => {
+    console.log(
+        `Aplicación inicializada... 
+        escuchando en el puerto ${PORT}.`
+    )
+})
